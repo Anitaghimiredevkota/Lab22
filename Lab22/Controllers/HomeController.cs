0bba238de8b13053eb49c8420f2285a3ee154ee5
Anitaghimiredevkota <arushighimire@gmail.com>
2019-05-28 08:31:16 -0400

    This is my first commit for lab 22

---
+++ Lab22/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab22.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //public ActionResult GetUserinfo(string firstname,string lastname,string email,string phonenumber,string password)
+        //{
+        //    ViewBag.firstname = firstname;
+        //    ViewBag.lastname = lastname;
+        //    ViewBag.email = email;
+        //    ViewBag.phonenumber = phonenumber;
+        //    ViewBag.password = password;
+
+        //    return View("SummaryPage");
+
+        //}
+        //public ActionResult SummaryPage()
+        //{
+        //    return View();
+        //}
+       public ActionResult SummaryPage(string firstname,string lastname, string email,string phonenumber, string password)
+        {
+            Models.User user = new Models.User();
+            user.Firstname = firstname;
+            user.Lastname = lastname;
+            user.Email = email;
+            user.Phonenumber = phonenumber;
+            user.Password = password;
+            return View(user);
+        }
+        
+    }
+}
---
+++ Lab22/Models/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab22.Models
+{
+    public class User
+    {
+        private string firstname;
+        private string lastname;
+        private string email;
+        private string phonenumber;
+        private string password;
+
+        public string Firstname
+        {
+            set { firstname=value; }
+            get { return firstname; }
+        }
+        public string Lastname
+        {
+            set {  lastname=value; }
+            get { return lastname; }
+        }
+        public string Email
+        {
+            set { email= value; }
+            get { return email; }
+        }
+        public string Phonenumber
+        {
+            set { phonenumber= value; }
+            get { return phonenumber; }
+        }
+        public string Password
+        {
+            set { password =value; }
+            get { return password; }
+        }
+        public User()
+        {
+
+        }
+        public User(string _firstname, string _lastname,string _email,string _phonenumber,string _password)
+        {
+            Firstname = _firstname;
+            Lastname = _lastname;
+            Email = _email;
+            Phonenumber = _phonenumber;
+            Password = _password;
+        }
+    }
+}
